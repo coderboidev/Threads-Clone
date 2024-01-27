33e8933296c60ecdc11e071dbe775909a8b19a02
Aditya Jawanjal <adityajawanjal01@gmail.com>
2024-01-27 14:50:23 +0530

    m

--- server/index.js
+++ server/index.js
@@ -9,12 +9,21 @@ dotenv.config();
 const app = express();
 connectDB();
 
+const allowCrossDomain = function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  next();
+};
+
 app.use(
   cors({
     origin: process.env.FRONTEND_URL,
     credentials: true,
   })
 );
+
+app.use(allowCrossDomain)
 app.use(express.json());
 app.use(cookieParser());
 app.use("/api", router);
